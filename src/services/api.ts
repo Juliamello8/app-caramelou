4d714c5ccb113baa3e7eaa5a33316537027fba6c
rafzsz <rafa.el@live.com>
2021-11-20 11:11:37 -0300

    conexao api rafa

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,8 @@ import { create } from 'apisauce'
 
 const api = create({
     // baseURL: 'http://localhost:3000',
-    // baseURL: 'http://192.168.1.14:3000',
-    baseURL: 'http://192.168.100.211:3000',
+    baseURL: 'http://192.168.1.14:3000',
+    // baseURL: 'http://192.168.100.211:3000',
 });
 
 // api.addResponseTransform(response => {
